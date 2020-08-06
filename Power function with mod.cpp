b2f8fd8e9c73f14bfc2282e1477913d3e329b99f
div5252 <54735797+div5252@users.noreply.github.com>
2020-08-06 09:58:25 +0530

    Create Power function with mod.cpp

---
+++ Power function with mod.cpp
@@ -0,0 +1,10 @@
+const int MOD=1000000007;
+
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
